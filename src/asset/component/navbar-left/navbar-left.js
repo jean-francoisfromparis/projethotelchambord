0afd7c92490fcde4c97764dda93e6133c0bb0f6e
nicolas <nicolas_zanardo@live.fr>
2021-04-29 22:01:29 +0200

    navbar _v1.00

--- src/asset/component/navbar-left/navbar-left.js
+++ src/asset/component/navbar-left/navbar-left.js
@@ -1,2 +1,32 @@
 const navbarLeft = document.getElementById("navbar-left");
-navbarLeft.innerHTML = `TODO Navbar Left`
+// init
+navbarLeft.style.left = "-250px";
+navbarLeft.appendChild(document.createElement("div")).innerHTML = `
+       <div class="search">   
+            <i class="fas fa-search"></i>
+            <input type="text" placeholder="SEARCH">
+       </div>
+       <div class="navLink"></div>
+    `;
+
+const menuArray = [
+    "accueil",
+    "chambre",
+    "restaurant",
+    "spa-soins",
+    "hotel"
+]
+const navLink= document.querySelector(".navLink");
+for (let item of menuArray) {
+    if(item === "accueil") {
+        navLink.appendChild( document.createElement("div")).innerHTML = `
+        <a href="index.html" alt="chambord palace hôtel de lux ${item}"><h1>${item}</h1></a>
+    `;
+    } else {
+        navLink.appendChild( document.createElement("div")).innerHTML = `
+        <a href="${item}.html" alt="chambord palace hôtel de lux ${item}"><h1>${item}</h1></a>
+    `;
+    }
+
+
+}
--- src/asset/component/navbar-top/navbar-top.js
+++ src/asset/component/navbar-top/navbar-top.js
@@ -1,2 +1,81 @@
+/**
+ * variable
+ */
+//HTML
 const navbarTop = document.getElementById("navbar-top");
-navbarTop.innerHTML = `TODO Navbar top`
+// Condition
+
+/**
+ * HTML
+ */
+let  languageLocalStorage = localStorage.getItem('language')
+if(!languageLocalStorage) {
+    localStorage.setItem('language', 'Fr');
+    languageLocalStorage = localStorage.getItem('language');
+} else {
+    languageLocalStorage = localStorage.getItem('language');
+}
+
+navbarTop.innerHTML = `
+<div class="d-flex justify-content-between align-items-center h-100 mx-2">
+    <div class="menu-btn">
+        <div class="sandwich">
+            <div class="open d-flex">
+                <i class="fas fa-bars fa-lg m-1 open-close"></i>
+                <div class="d-flex justify-content-center m-1">
+                    <h5>open</h5>
+                </div>
+            </div>
+            <div class="close d-none d-flex">
+                <i class="fas fa-times-circle fa-lg m-1 open-close"></i>
+                <div class="d-flex justify-content-center m-1">
+                    <h5>close</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="/index.html">
+        <div class="logo">
+        <img class="img-logo" src="./asset/images/logo.png" alt="palace de luxe hôtel chambord">
+        <h1 class="py-0 m-0 text-center title">CHAMBORD</h1>
+        <h6 class="py-0 m-0 text-center sub-title" >hotel d'exellence à paris</h6>
+    </div>
+    </a>
+    <div class="d-flex menu-option">
+        <div class="d-flex align-items-center"><a href="#"><i class="fas fa-gem fa-sm mx-1"></i><span class="text-price">0</span><span class="fw-bold fs-6 mx-1 text-price">€</span></div></a>
+        <span class="separator"><div>|</div></span>
+        <div class="d-flex justify-content-center block-language"></a><i class="fas fa-globe m-1"></i><span class="language">${languageLocalStorage}</span></div>
+    </div>
+    <div id="modal-search"></div>
+    <div id="modal-sandwich"></div>
+</div>`
+
+/**
+ * Events
+ */
+// Sandwich menu-left
+document.querySelector(".sandwich").addEventListener("click", ()=> {
+    let navLet = document.getElementById("navbar-left");
+    const open = document.querySelector(".open");
+    const close = document.querySelector(".close");
+    if (navLet.style.left === "-250px") {
+        navLet.style.left = "0px";
+        open.classList.add("d-none");
+        close.classList.remove("d-none");
+    } else {
+        navLet.style.left = "-250px";
+        open.classList.remove("d-none");
+        close.classList.add("d-none");
+    }
+})
+// language
+document.querySelector(".block-language").addEventListener('click', ()=> {
+    const language = document.querySelector(".language");
+    if(language.textContent === "Fr") {
+        language.textContent = "En";
+        localStorage.setItem('language', 'En');
+    } else {
+        language.textContent = "Fr";
+        localStorage.setItem('language', 'Fr');
+    }
+})
