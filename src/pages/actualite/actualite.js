831191f06ea460fb9c7d56ae6c3b1f24c4709e67
nicolas <nicolas_zanardo@live.fr>
2021-04-30 10:51:09 +0200

    refactoring exemple

--- src/pages/actualite/actualite.js
+++ src/pages/actualite/actualite.js
@@ -1,6 +1,7 @@
 import "bootstrap";
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import "../../asset/styles/styles.scss";
+import "actualite.scss";
