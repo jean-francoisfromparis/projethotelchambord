0421c080e738cdc96c28a11558489b900b1b293c
ciceron1971 <jean-francois.lepante@live.fr>
2021-05-04 17:48:36 +0200

    sauvegarde feature_presse refact config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ module.exports = {
                 }],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif|pdf|mp4)$/i,
                 type: 'asset/resource',
             },
             {
